7e1b6f931fb82e2bd3e6fcd02aa0fb0412cdab9a
Ivan Bibinin <>
2022-04-26 23:53:00 +0400

    подключил картинки, чуть стилизовал карточки

--- src/components/FilmsCard/FilmCard.vue
+++ src/components/FilmsCard/FilmCard.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="film">
-    <div class="film_name body-17 no-margin">
-      {{ film.title}}
+    <div class="film_image-wrapper">
+      <img class="film_img" :src="itemImage"
+           :alt="film.title">
+    </div>
+    <div class="film_description">
+      <div class="film_name body-17 no-margin">
+        {{ film.title }}
+      </div>
     </div>
   </div>
 </template>
@@ -10,5 +16,15 @@
 export default {
   name: 'FilmCard',
   props: ['film'],
+  data() {
+    return {
+      posterNumber: 1,
+    };
+  },
+  computed: {
+    itemImage() {
+      return `/img/posters/poster-${this.film.episode_id}.jpg`;
+    },
+  },
 };
 </script>
